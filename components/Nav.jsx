b3a5ed5b373b206ff941b1f8792a1b91082f09a9
Akhtar <iaktr999@gmail.com>
2023-12-21 10:29:57 +0530

    navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link"; // it will allow us to move to other pages of our app
+import Image from "next/image"; // it is going to automatically optimize the image for us
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="PrompTron logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">PrompTron</p>
+      </Link>
 
-export default Nav
+      {/* mobile navigation */}
+       <div></div>
+    </nav>
+  );
+};
 
+export default Nav;
